eb68925742e733657b6eb7052e506e7c1061baf4
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-27 20:07:16 +0100

    Creating the blog card component

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,12 +3,17 @@ import Image from 'next/image';
 import styles from '../styles/BlogCard.module.css';
 
 const BlogCard = ({ key, title, datePublished, slug, coverPhoto, author }) => {
-	console.log(author);
 	return (
 		<div className={styles.card}>
 			<Link href={'/posts/' + slug}>
 				<div className={styles.imgContainer}>
-					{/* <Image src={coverPhoto} alt={title} /> */}
+					{/* <Image
+						src={coverPhoto.url}
+						alt={title}
+						width={376}
+						height={190}
+						layout="fixed"
+					/> */}
 					<img src={coverPhoto.url} alt={title} />
 				</div>
 			</Link>
@@ -16,7 +21,11 @@ const BlogCard = ({ key, title, datePublished, slug, coverPhoto, author }) => {
 				<h2>{title}</h2>
 				<div className={styles.details}>
 					<div className={styles.author}>
-						<img src={author.avatar.url} alt="" />
+						<img src={author.avatar.url} alt={author.name} />
+						<h3>{author.name}</h3>
+					</div>
+					<div className={styles.date}>
+						<h4>{datePublished}</h4>
 					</div>
 				</div>
 			</div>
